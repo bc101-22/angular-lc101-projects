e294f0eccbfc874e2e17aaa74cf00cfcf2615214
bc101-22 <chensi2021@gmail.com>
2022-12-08 16:16:25 -0500

    angular exercise1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
+  image2 = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.krXgDcts2hh2AT61oGCDrgHaE8%26pid%3DApi&f=1&ipt=8233b5e96a876b80a477694c821bb1233657ac195bf99e4ee923a35372b8d3eb&ipo=images';
   image3 = '';
 
   constructor() { }
